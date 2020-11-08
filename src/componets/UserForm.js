0c19aa2e29052c06ff2761ba1504141cbb2f3d12
Narayan Pandey <npandey722@gmail.com>
2020-11-08 20:51:38 +0530

    UserForm

--- src/componets/UserForm.js
+++ src/componets/UserForm.js
@@ -1,14 +1,72 @@
 import React, { Component } from 'react'
+import FormPersonalDetails from './FormPersonalDetails';
+import FormUserDetails from './FormUserDetails';
+import Success from './Success';
+import Confirm from './Confirm';
 
 export class UserForm extends Component {
+    state={
+        step:1,
+        firstName:'',
+        lastName:'',
+        email:'',
+        occupation:'',
+        city:'',
+        bio:''
+
+    }
+    // proceed to next step 
+
+    nextStep = () => {
+        const {step} = this.state;
+        this.setState({
+            step:step+1
+        })
+    }
+
+    //go back to prev state
+
+    nextStep = () => {
+        const {step} = this.state;
+        this.setState({
+            step:step-1
+        })
+    }
+    //Handle fields change 
+
+    handleChange = input => e =>{
+        this.setState({
+            [input]:e.target.value
+        })
+    }
     render() {
-        return (
-            <div>
-                
-            </div>
-        )
+        const {step} = this.state;
+        const {firstName, lastName,email, occupation, city, bio} = this.state; 
+        const values = {firstName, lastName,email, occupation, city, bio}
+       switch(step) {
+            case 1:
+                return (
+                    <FormUserDetails
+                    nextStep = {this.nextStep}
+                    handleChange={this.handleChange}
+                    values={values}                    
+                    />
+                )
+            case 2:
+                return (
+                    <FormPersonalDetails />
+                )
+            case 3:
+                    return (
+                        <Confirm />
+                    )
+            case 3:
+                    return (
+                         <Success />
+                    )
+       }
     }
 }
 
-export default UserForm
+export default UserForm;
 
