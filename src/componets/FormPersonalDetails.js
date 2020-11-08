5b0457efbcb36f85b3eaadb0abec097e0cb99bc7
Narayan Pandey <npandey722@gmail.com>
2020-11-08 12:46:36 +0530

    components

---
+++ src/componets/FormPersonalDetails.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function FormPersonalDetails() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default FormPersonalDetails
---
+++ src/componets/FormUserDetails.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function FormUserDetails() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default FormUserDetails
